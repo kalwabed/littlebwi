49848fe673c7df40cb8d4a5fa4990ab0449438e3
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-06-14 21:08:38 +0700

    feat: add logo

--- src/defaultSeo.ts
+++ src/defaultSeo.ts
@@ -14,6 +14,12 @@ const defaultSeo: DefaultSeoProps = {
     url: appConfig.siteUrl,
     type: 'website'
   },
+  additionalLinkTags: [
+    { rel: 'icon', href: '/favicon.ico' },
+    { rel: 'icon', href: '/icon.svg', type: 'image/svg+xml' },
+    { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+    { rel: 'manifest', href: '/manifest.webmanifest' }
+  ],
   twitter: { cardType: 'summary_large_image', handle: '@kalwabed', site: appConfig.siteUrl }
 }
 
