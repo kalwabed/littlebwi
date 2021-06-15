0a97d70c98b0ebdc01ce0a2c4b1254a4dffca4c7
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-06-15 20:10:42 +0700

    feat: switch to preact while in production

--- next.config.js
+++ next.config.js
@@ -8,5 +8,16 @@ module.exports = {
       dev: true,
       build: false
     }
+  },
+  webpack: (config, { isServer, dev }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom': 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils'
+      })
+    }
+
+    return config
   }
 }
